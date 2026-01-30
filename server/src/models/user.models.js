72a3cecee937dfb289129f919a1e0354ca50d938
Pranita Kawadkar <pranitakawadkar10@gmail.com>
2026-01-30 13:49:21 +0530

    feat(user-model): add User mongoose schema with roles and timestamps

---
+++ server/src/models/user.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "admin", "teacher"],
+      default: "student",
+    },
+  },
+  { timestamps: true },
+);
+
+export const userModel = mongoose.model("User", userSchema);
